c4748cdf41233ba2d8c873eedf6248f640451dd6
Davit <73256537+Davit-Dzidziguri@users.noreply.github.com>
2023-10-07 16:04:35 +0400

    Add files via upload

---
+++ web.php
@@ -0,0 +1,60 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return "Information about me";
+});
+
+Route::get('firstName', function () {
+    return "Davit";
+});
+
+Route::get('lastName', function () {
+    return "Dzidziguri";
+});
+
+Route::get('age', function () {
+    return "21";
+});
+
+Route::get('hobby', function () {
+    return "Playing basketball";
+});
+
+Route::get('currentUniversity', function () {
+    return "BTU";
+});
+
+Route::get('currentUniversity', function () {
+    return "BTU";
+});
+
+Route::post("/save", function () {
+    return response()->json([
+        "msg"=> "წარმატებით განახლდა"
+    ]);
+});
+
+Route::put("/save", function () {
+    return response()->json([
+        "msg"=> "წარმატებით დაემატა"
+    ]);
+});
+
+Route::delete("/save", function () {
+    return response()->json([
+        "msg"=> "წარმატებით წაიშალა"
+    ]);
+});
